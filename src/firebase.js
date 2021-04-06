57f468ce29e169e1f154b8d9a3c26a17ce1b776a
subratoroy1 <roy.subrato@gmail.com>
2021-04-06 19:14:50 +0100

    anonymise upload

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0kpLvddRDD6AawbU9y3ucgJvqKl7EAvU",
-  authDomain: "credsverifynet.firebaseapp.com",
+  apiKey: "xxxxxxxxxxxxxxxxxxxx",
+  authDomain: "xxxxxxxx.firebaseapp.com",
   projectId: "credsverifynet",
   storageBucket: "credsverifynet.appspot.com",
-  messagingSenderId: "620384758669",
-  appId: "1:620384758669:web:f81d0af4dcebc4fa2cebc4",
-  measurementId: "G-EEFLPVZ193",
+  messagingSenderId: "111111111111",
+  appId: "xxxxxxxxxxx",
+  measurementId: "xxxxxxxxx",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
